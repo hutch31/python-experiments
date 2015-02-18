e08b55564ad18807ccc02e8cc9bda6a991a78514
Guy Hutchison <guy@xpliant.com>
2015-02-17 17:40:32 -0800

    Added gfmul sample algorithm, updated route learning

--- route/route.py
+++ route/route.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-import math, random
+import math
+import random
+import numpy
+
 
 # Define a routing grid
 # Origin 0, 0 is in lower-left corner
@@ -8,10 +11,10 @@ class rgrid:
     def __init__(self, x=10, y=10):
         self.x_dim = x
         self.y_dim = y
-        self.grid  = [[0] * x for yp in range(y)]
+        self.grid = numpy.array([[0] * x for yp in range(y)])
 
-    def display (self):
-        for y in range(self.y_dim):
+    def display(self):
+        for y in range(self.y_dim-1, -1, -1):
             line = ''
             for x in range(self.x_dim):
                 if self.grid[x][y] == 0:
@@ -19,6 +22,13 @@ class rgrid:
                 else:
                     line += '*'
             print line
+
+    def op_or(self, rg):
+        if rg.x_dim != self.x_dim or rg.y_dim != self.y_dim:
+            return None
+        rv = rgrid(self.x_dim, self.y_dim)
+        rv.grid = self.grid or rg.grid
+        return rv
 
 class rbfnet:
     def __init__(self, dim=2, func=2, outputs=2):
@@ -45,16 +55,15 @@ class rbfnet:
             temp = 0.0
             for cj in range(self.dim):
                 temp += math.pow(self.center[ci][cj] - inputs[cj], 2)
-            fval.append (math.pow(math.e, -temp))
+            fval.append(math.pow(math.e, -temp))
 
         out = []
         for o in range(self.outputs):
             temp = 0.0
             for f in range(self.func):
                 temp += self.weight[o][f] * fval[f]
-            out.append (temp)
+            out.append(temp)
         return out
-
 
 
 class router:
@@ -62,12 +71,17 @@ class router:
         self.grid = rg
         self.cur = (0, 0)
         self.target = (0, 0)
+        self.rbf = rbfnet(6, 4, 4)
+        self.rbf.randomize()
 
     def set_position(self, x, y):
         self.cur = (x, y)
 
     def set_target(self, x, y):
         self.target = (x, y)
+
+    def manhattan(self):
+        return abs(self.target[0]-self.cur[0])+abs(self.target[1]-self.cur[1])
 
     # determine if path is blocked in any of 4 directions
     # returns [West, North, East, South]
@@ -85,35 +99,107 @@ class router:
             bvec[1] = True
 
         # Set blocked if grid is used 1 space away
-        if not bvec[0] and self.grid.grid[self.cur[0]-1][self.cur[1]] != 0:
+        if not bvec[0] and self.cur[0] > 0 and self.grid.grid[self.cur[0]-1][self.cur[1]] != 0:
             bvec[0] = True
-        if not bvec[1] and self.grid.grid[self.cur[0]][self.cur[1]+1] != 0:
-            bvec[1] = True
-        if not bvec[2] and self.grid.grid[self.cur[0]+1][self.cur[1]] != 0:
+        elif not bvec[2] and self.cur[0] < (self.grid.x_dim-1) and self.grid.grid[self.cur[0]+1][self.cur[1]] != 0:
             bvec[2] = True
-        if not bvec[3] and self.grid.grid[self.cur[0]][self.cur[1]-1] != 0:
+        if not bvec[1] and self.cur[1] < (self.grid.y_dim-1) and self.grid.grid[self.cur[0]][self.cur[1]+1] != 0:
+            bvec[1] = True
+        elif not bvec[3] and self.cur[1] > 0 and self.grid.grid[self.cur[0]][self.cur[1]-1] != 0:
             bvec[3] = True
 
-        # Set blocked if grid is used 2 space away
-        # Need edge of grid check
-        if not bvec[0] and self.grid.grid[self.cur[0]-2][self.cur[1]] != 0:
+        # Set blocked if grid is used 1 space away
+        if not bvec[0] and self.cur[0] > 1 and self.grid.grid[self.cur[0]-2][self.cur[1]] != 0:
             bvec[0] = True
-        if not bvec[1] and self.grid.grid[self.cur[0]][self.cur[1]+2] != 0:
-            bvec[1] = True
-        if not bvec[2] and self.grid.grid[self.cur[0]+2][self.cur[1]] != 0:
+        elif not bvec[2] and self.cur[0] < (self.grid.x_dim-2) and self.grid.grid[self.cur[0]+2][self.cur[1]] != 0:
             bvec[2] = True
-        if not bvec[3] and self.grid.grid[self.cur[0]][self.cur[1]-2] != 0:
+        if not bvec[1] and self.cur[1] < (self.grid.y_dim-2) and self.grid.grid[self.cur[0]][self.cur[1]+2] != 0:
+            bvec[1] = True
+        elif not bvec[3] and self.cur[1] > 1 and self.grid.grid[self.cur[0]][self.cur[1]-2] != 0:
             bvec[3] = True
+
+
+        # # Set blocked if grid is used 2 space away
+        # # Need edge of grid check
+        # if not bvec[0] and self.grid.grid[self.cur[0]-2][self.cur[1]] != 0:
+        #     bvec[0] = True
+        # if not bvec[1] and self.grid.grid[self.cur[0]][self.cur[1]+2] != 0:
+        #     bvec[1] = True
+        # if not bvec[2] and self.grid.grid[self.cur[0]+2][self.cur[1]] != 0:
+        #     bvec[2] = True
+        # if not bvec[3] and self.grid.grid[self.cur[0]][self.cur[1]-2] != 0:
+        #     bvec[3] = True
 
         return bvec
 
-rg = rgrid()
-rg.display()
+    def mark(self):
+        self.grid.grid[self.cur[0]][self.cur[1]] = 1
 
-r = router(rg)
-print r.blocked()
+    # move in a direction, directions are 0=West,1=North,2=East,3=South
+    def move(self, dir=0):
+        bvec = self.blocked()
+        if bvec[dir]:
+            return
+        else:
+            print "Debug: cur",self.cur,"move",dir,"blocked",bvec
+            if dir == 0:
+                self.cur = (self.cur[0]-1, self.cur[1])
+            elif dir == 1:
+                self.cur = (self.cur[0], self.cur[1]+1)
+            elif dir == 2:
+                self.cur = (self.cur[0]+1, self.cur[1])
+            elif dir == 3:
+                self.cur = (self.cur[0], self.cur[1]-1)
+            self.mark()
 
-# create rbf network
-rbf = rbfnet(2, 3, 4)
-rbf.randomize()
-print rbf.eval ([0.0,0.0])
+    def do_route(self):
+        # normalize x delta
+        delta_x = ((self.target[0]-self.cur[0])/float(self.grid.x_dim)/2.0+0.5)
+        delta_y = ((self.target[1]-self.cur[1])/float(self.grid.y_dim)/2.0+0.5)
+        trials = self.manhattan()*3
+        print "Manhattan distance", self.manhattan()
+        self.mark()
+        while trials > 0 and self.target != self.cur:
+            inputs = [delta_x, delta_y]+self.blocked()
+            result = self.rbf.eval(inputs)
+            movedir = numpy.argmax(result)
+            print "Trials", trials, "result", result, "movedir=", movedir
+            self.move(movedir)
+            trials -= 1
+
+    def score(self):
+        s = 0
+        if self.cur == self.target:
+            s += 100
+        else:
+            s += 20 - self.manhattan()
+        return s
+
+
+def training_loop():
+    max_score = 0
+    best_rbf = None
+
+    for t in range(1000):
+        rg = rgrid()
+        rg.grid[0][3] = 2
+
+        r = router(rg)
+        r.target = (7, 7)
+        r.do_route()
+        r.grid.display()
+
+        if r.score() > max_score:
+            print "Updating new best score", r.score()
+            best_rbf = r.rbf
+            max_score = r.score()
+
+    rg = rgrid()
+    rg.grid[0][3] = 2
+    r = router(rg)
+    r.rbf = best_rbf
+    r.target = (7, 7)
+    r.do_route()
+    r.grid.display()
+
+training_loop()
