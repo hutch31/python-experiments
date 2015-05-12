aaeaa4f581065a5b7e0d50b9646bb4952b02bbd0
Guy Hutchison <ghutchison@caviumnetworks.com>
2015-05-12 11:57:06 -0700

    Added self-checking test to gcm and travis definition file

--- gcm/gfmul.py
+++ gcm/gfmul.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import unittest
+import numpy
+
 gfpoly = 0xe1 << 120
 
 def gfmul(x, y):
@@ -14,12 +17,20 @@ def gfmul(x, y):
             v = v >> 1
     return z
 
-h = 0x66e94bd4ef8a2c3b884cfa59ca342b2eL
-c = 0x0388dace60b6a392f328c2b971b2fe78L
-exphash = 0xf38cbb1ad69223dcc3457ae5b6b0f885L
+class TestPoly(unittest.TestCase):
+    def test_poly1(self):
+        h = 0x66e94bd4ef8a2c3b884cfa59ca342b2eL
+        c = 0x0388dace60b6a392f328c2b971b2fe78L
+        exphash = 0xf38cbb1ad69223dcc3457ae5b6b0f885L
 
-r1 = gfmul(h, c)
-r2 = gfmul(h, r1 ^ 0x80)
+        r1 = gfmul(h, c)
+        r2 = gfmul(h, r1 ^ 0x80)
 
-print "%x" % r2
-print "%x" % exphash
+        self.assertEquals(r2, exphash)
+
+    def test_zero(self):
+        r = gfmul(0, 0)
+        self.assertEquals(r, 0)
+
+if __name__ == '__main__':
+    unittest.test()
